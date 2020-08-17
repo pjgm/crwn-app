8dfeed8698376afa08195afa0d2e3d9664975041
Paulo Martins <paulojgmartins92@gmail.com>
2020-08-17 21:56:20 +0100

    Updates

---
+++ precache-manifest.76f4fe98b5a289c537429e070398b693.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c1419b6e8fdc7dd7fcb882a6dd63be",
+    "url": "/crwn-app/index.html"
+  },
+  {
+    "revision": "69e9fc820048cac2fca1",
+    "url": "/crwn-app/static/css/main.0f29d0dc.chunk.css"
+  },
+  {
+    "revision": "a7b16ee89b8f880f098f",
+    "url": "/crwn-app/static/js/2.7e8df3ef.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/crwn-app/static/js/2.7e8df3ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69e9fc820048cac2fca1",
+    "url": "/crwn-app/static/js/main.579f2729.chunk.js"
+  },
+  {
+    "revision": "5aef93b8e3ed445827e4",
+    "url": "/crwn-app/static/js/runtime-main.1b65551e.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-app/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-app/static/media/shopping-bag.30f07071.svg"
+  }
+]);
